4ad4053e1dd876122e878bd42663bb647d0707d6
yanlele <331393627@qq.com>
2019-04-03 19:01:48 +0800

    fix(remove): remove js and js.map file

--- src/demo/01、test.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var UserAgent = require("user-agents");
-var userAgent = new UserAgent();
-console.log(userAgent.toString());
-//# sourceMappingURL=01、test.js.map
