ae14bc2a0f434ad578d2b459edd80fb3c513c6e9
yanlele <331393627@qq.com>
2019-04-09 11:04:02 +0800

    feat(commitlint): change commitlint rule

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,6 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional'],
+  extends: ["@commitlint/config-conventional"],
   rules: {
-    'header-max-length': [0],
-    'scope-enum': [2, 'always', ['root', 'leech', 'armory']],
+    "header-max-length": [0],
   },
 };
